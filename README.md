dd97375dd93f48e50a92a9b88388a2f82570560a
Alex <adkipnis@mailbox.org>
2021-03-22 20:17:33 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# take-it-easy
+Implementation of the [Take it Easy!](https://boardgamegeek.com/boardgame/128/take-it-easy) boardgame in Python for a hackathon.
---
+++ main.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Take it easy
+"""
+import itertools as it
+import numpy as np
+
+class TIE():
+# Initialization    
+    def __init__(self):
+        # setup fields
+        field_full = np.array([(i,j) for i in range(5) for j in range(5)])
+        empty_idx = [3, 4, 9, 19, 23, 24]
+        self.field_orig = np.delete(field_full, empty_idx, axis=0)
+        self.field = self.field_orig.copy()
+        
+        #diagonals
+        self.diag_r = [self.field[self.field[:,0]==i] for i in range(5)]
+        self.diag_l = [self.field[self.field[:,1]==i] for i in range(5)]
+        
+        # columns
+        c_1 = np.array([[i,0] for i in range(2,5)])
+        c_2 = np.concatenate(([[1,0]], [[i,1] for i in range(2,5)]))
+        c_3 = np.concatenate(([[i,i] for i in range(3)], [[3,2], [4,2]]))
+        c_4 = np.array([[0+i,1+i] for i in range(4)])
+        c_5 = np.array([[0+i,3+i] for i in range(3)])
+        self.cols = [c_1, c_2, c_3, c_4, c_5]
+        
+        # setup cards:
+        set_u = {1,5,9}
+        set_l = {2,6,7}
+        set_r = {3,4,8}
+        self.deck = np.array([(i) for i in it.product(set_u, set_l, set_r)])
+        
+        # rest
+        self.limbo = []
+        self.placed = []
+        self.score = 0
+        
+# Basic Actions
+    def draw_card(self):
+        if len(self.limbo) == 0:
+            choice_idx = np.random.choice(len(self.deck), 1)
+            self.limbo = self.deck[choice_idx]
+            self.show_drawn()
+            self.deck = np.delete(self.deck, choice_idx, axis=0)
+        else:
+            print("You must first place your drawn card")
+    
+    def random_pos(self):
+        choice_idx = np.random.choice(len(self.field), 1)
+        return self.field[choice_idx]
+        
+    def place_card(self, pos = None):
+        assert len(self.field) > 0, "No more cards to draw."
+        
+        if pos is None:
+            pos = self.random_pos()
+        print("Position:", pos[0])
+        
+        if pos in self.field:
+            self.field = np.delete(
+                self.field,
+                np.argwhere(np.all(pos == self.field, axis=1)),
+                axis=0)
+            self.placed.append((self.limbo[0], pos[0]))
+            self.limbo = []
+        else:
+            print("The chosen position is not available.")
+    
+    def single_round(self, pos = None):
+        self.draw_card()
+        self.place_card(pos = pos)
+    
+    def random_game(self):
+        while len(self.field) > 0:
+            self.single_round()
+
+# Evaluation   
+    def eval_streak(self, bar, number_idx):
+        bar_score = 0
+        for col in bar:
+            streak = []
+            for turn in self.placed:
+                if np.any(np.all(turn[1] == col, axis = 1)):
+                    streak.append(turn[0][number_idx])
+            if len(np.unique(streak))==1:
+                bar_score += np.sum(streak)
+        print("Points:", bar_score) 
+        self.score += bar_score
+         
+    def eval_score(self):
+        self.eval_streak(self.cols, number_idx = 0)
+        self.eval_streak(self.diag_l, number_idx = 1)
+        self.eval_streak(self.diag_r, number_idx = 2)
+        print("Points total:", self.score) 
+
+# Getter functioins      
+    def show_deck(self):
+        return self.deck.copy()
+    
+    def show_drawn(self):
+        if len(self.limbo)>0:
+            print("Card:", self.limbo[0])
+            
+##############################################################################
+
+game = TIE()
+game.show_deck()
+game.random_game()
+game.eval_score()
